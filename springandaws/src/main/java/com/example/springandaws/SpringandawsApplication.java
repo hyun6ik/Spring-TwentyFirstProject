bba3a595d04f6027667aa1bfd59ca24e66027ffe
hyun6ik <fkanwm@naver.com>
2021-03-23 18:47:20 +0900

    프로젝트 생성

---
+++ springandaws/src/main/java/com/example/springandaws/SpringandawsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springandaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringandawsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringandawsApplication.class, args);
+	}
+
+}
---
+++ springandaws/src/test/java/com/example/springandaws/SpringandawsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springandaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringandawsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
