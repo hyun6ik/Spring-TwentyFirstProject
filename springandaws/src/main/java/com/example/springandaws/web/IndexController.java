6575520ac6c1cdb74c96bb61577b2b7d01de6bfa
hyun6ik <fkanwm@naver.com>
2021-03-27 22:33:40 +0900

    게시글 삭제 페이지

--- springandaws/src/main/java/com/example/springandaws/web/IndexController.java
+++ springandaws/src/main/java/com/example/springandaws/web/IndexController.java
@@ -6,6 +6,7 @@ import com.example.springandaws.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -33,7 +34,6 @@ public class IndexController {
 
         return "posts-update";
     }
-
 
 
 }
