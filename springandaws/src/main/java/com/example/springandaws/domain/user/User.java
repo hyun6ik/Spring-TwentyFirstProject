0ea06548b91ee4dbb12d5c538ad643facd6e8f32
hyun6ik <fkanwm@naver.com>
2021-03-27 23:34:07 +0900

    User Entity,Role,Repository 생성

---
+++ springandaws/src/main/java/com/example/springandaws/domain/user/User.java
@@ -0,0 +1,50 @@
+package com.example.springandaws.domain.user;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column
+    private String pictures;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Builder
+    public User(String name, String email, String pictures, Role role) {
+        this.name = name;
+        this.email = email;
+        this.pictures = pictures;
+        this.role = role;
+    }
+
+    public User update(String name, String pictures){
+        this.name = name;
+        this.pictures = pictures;
+
+        return this;
+    }
+
+    public String getRoleKey(){
+        return this.role.getKey();
+    }
+}
